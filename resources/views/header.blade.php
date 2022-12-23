555026d56e65a7669be89c80ddbd0c52de93462f
Alexandr <a_berdyugin@mail.ru>
2022-12-23 11:53:42 +0600

    new mail

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,7 +17,7 @@
                     <li class="address">
                         <i class="icon flaticon-multimedia"></i>
                         <h6>Почта:</h6>
-                        <p>suppor@codexconsult.kz</p>
+                        <p>info@codexconsult.kz</p>
                     </li>
                 {{--<li class="quotes"><a href="index">Задать вопрос</a></li> --}}
                 </ul>
