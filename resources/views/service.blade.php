ae6965b5403b65f84c5c8837bdfaeee9f302ce09
alexey <sulimenko@ptfin.kz>
2022-12-28 11:45:09 +0600

    style services

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -2,9 +2,9 @@
     <div class="single-service">
         <div class="img-box"><img src="images/home/{{ $each['image'] }}" alt=""></div>
         <div class="text">
-            <h5><a href="service-details">{{ $each['details'] }}</a></h5>
+            <a href="service-details">{{ $each['details'] }}</a>
             {{-- <p>The tiny ship today stiller</p> --}}
-            <a href="service-details" class="read-more">удалить <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            {{-- <a href="service-details" class="read-more">удалить <i class="fa fa-angle-right" aria-hidden="true"></i></a> --}}
         </div> <!-- /.text -->
     </div> <!-- /.single-service -->
 </div> <!-- /.col- -->
